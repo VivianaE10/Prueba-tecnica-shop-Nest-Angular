d4c3b0c2ef10eeb97cb4d334771618f3268c3455
Viviana Ospina <vyvy_escobar@hotmail.com>
2026-01-20 13:56:40 -0500

    - Se instaló la librería @nestjs/config para manejo de variables de entorno. - Se creó el archivo .env con las variables de entorno. - Se actualizó AppModule para cargar ConfigModule globalmente. - TypeOrmModule configurado con TypeOrmModule.forRootAsync() usando las variables del archivo .env.

---
+++ database/shop.sql
@@ -0,0 +1,21 @@
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    category_id INT NOT NULL,
+    CONSTRAINT fk_products_categories
+        FOREIGN KEY (category_id)
+        REFERENCES categories(id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [
+    // Carga variables de entorno (.env)
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Configuración de TypeORM usando variables de entorno
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: Number(configService.get('DB_PORT')),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false, // IMPORTANTE 👀 debe de estar en false para que no me cambie nada ni las tablas ni los datos
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
