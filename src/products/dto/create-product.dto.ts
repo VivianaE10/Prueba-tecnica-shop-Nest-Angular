1370dd9f34eeff905dfceef92a99ce2efe831f48
Viviana Ospina <vyvy_escobar@hotmail.com>
2026-01-22 10:12:35 -0500

    agrega validaciones con class-validator en DTO de productos en el archivo create-productsdto.ts  y configura Validaciones  globalmente en main.ts

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,8 +1,35 @@
+import {
+  IsString,
+  IsOptional,
+  IsNumber,
+  IsInt,
+  Min,
+  IsPositive,
+  IsBoolean,
+} from 'class-validator';
+
+//descoradores para validar los datos de entrada
 export class CreateProductDto {
+  @IsString() //validacion obligatoria de string name
   name: string;
+
+  @IsOptional() //descripcion es opcional
+  @IsString() // debe ser string si se envia
   description?: string;
+
+  @IsNumber() //debe ser numero price obligatorio
+  @IsPositive() // mayor que 0
   price: number;
+
+  @IsInt() //debe ser entero stock obligatorio
+  @Min(0) // minimo 0
   stock: number;
+
+  @IsOptional() // es opcional
+  @IsBoolean() // debe ser booleano si se envia
   is_active?: boolean;
+
+  @IsInt() // debe ser entero
+  @IsPositive() // debe ser positivo
   category_id: number; // obligatorio debe existir en categorias
 }
